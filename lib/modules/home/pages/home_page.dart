aa66719877fc5911d20715be15c3b07104333f52
Harley Italo Soares Xavier <harleyitalo@hotmail.com>
2023-09-01 16:27:11 -0300

    Creating home page.

--- lib/modules/home/pages/home_page.dart
+++ lib/modules/home/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +14,77 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("SimpleBank"),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Theme.of(context).colorScheme.background,
+            Theme.of(context).colorScheme.primaryContainer,
+            Theme.of(context).colorScheme.primaryContainer,
+            Theme.of(context).colorScheme.primary,
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(
+              CupertinoIcons.building_2_fill,
+              size: 80,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "Welcome to SimpleBank",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+            Text(
+              "Here all is always simple",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 10),
+              child: Text(
+                "Do you have an account? If not, create one now!",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: FilledButton(
+                  onPressed: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      "Create your account",
+                    ),
+                  )),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                "Acess you account",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: OutlinedButton(
+                  onPressed: () {},
+                  child: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text("Access your account"),
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
